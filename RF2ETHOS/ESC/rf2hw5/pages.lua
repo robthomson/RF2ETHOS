df5fb6b1eaad000c01c11150004a9dd741acd515
Rob Thomson <rob@marotori.com>
2024-05-29 16:06:50 +0100

    Clean

--- RF2ETHOS/ESC/rf2hw5/pages.lua
+++
@@ -1,9 +0,0 @@
-local PageFiles = {}
-
--- ESC pages.
-PageFiles[#PageFiles + 1] = { title = "Basic Setup", script = "esc_basic.lua" }
-PageFiles[#PageFiles + 1] = { title = "Advanced Setup", script = "esc_advanced.lua" }
-PageFiles[#PageFiles + 1] = { title = "Other Settings", script = "esc_other.lua" }
--- PageFiles[#PageFiles + 1] = { title = "ESC Debug", script = "esc_debug.lua" }
-
-return PageFiles
--- RF2ETHOS/TEMPLATES/784x406.lua
+++
@@ -1,2 +0,0 @@
-return {margin = 0, indent = 15, lineSpacing = 45, listSpacing = {line = 45, field = 465}, tableSpacing = {row = 50, col = 150, header = 45}}
-
