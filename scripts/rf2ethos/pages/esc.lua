466f8ba07880563680dafe034e44b28e5de3a0df
Rob Thomson <rob@marotori.com>
2024-07-05 09:11:43 +0100

    Remove 'old msp version'

--- scripts/rf2ethos/pages/esc.lua
+++
@@ -1,8 +0,0 @@
-
-local pages = {}
-
-pages[#pages + 1] = {title = "SCORPION", folder = "rf2scorp", image = "scorpion.png"}
-pages[#pages + 1] = {title = "HOBBYWING 5", folder = "rf2hw5", image = "hobbywing.png"}
-pages[#pages + 1] = {title = "YGE", folder = "rf2yge", image = "yge.png"}
-
-return {title = "ESC", pages = pages}
--- scripts/rf2ethos/templates.na/632x314.lua
+++
@@ -1,1 +0,0 @@
-return {margin = 5, indent = 15, lineSpacing = 30, listSpacing = {line = 35, field = 220}, tableSpacing = {row = 35, col = 80, header = 27}}
