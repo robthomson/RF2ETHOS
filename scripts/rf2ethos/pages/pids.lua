bc15034d53262f0349a135c0c918449913a4fbb2
Rob Thomson <rob@marotori.com>
2024-08-12 12:03:47 +0100

    Improved PID Defaults

--- scripts/rf2ethos/pages/pids.lua
+++ scripts/rf2ethos/pages/pids.lua
@@ -10,21 +10,21 @@ cols = {"P", "I", "O", "D", "F", "B"}
 -- P
 fields[#fields + 1] = {help = "profilesProportional", row = 1, col = 1, min = 0, max = 1000, default = 50, vals = {1, 2}}
 fields[#fields + 1] = {help = "profilesProportional", row = 2, col = 1, min = 0, max = 1000, default = 50, vals = {9, 10}}
-fields[#fields + 1] = {help = "profilesProportional", row = 3, col = 1, t = "PY", min = 0, max = 1000, default = 50, vals = {17, 18}}
+fields[#fields + 1] = {help = "profilesProportional", row = 3, col = 1, t = "PY", min = 0, max = 1000, default = 80, vals = {17, 18}}
 
 -- I
-fields[#fields + 1] = {help = "profilesIntegral", row = 1, col = 2, min = 0, max = 1000, default = 100, vals = {3, 4}}
-fields[#fields + 1] = {help = "profilesIntegral", row = 2, col = 2, min = 0, max = 1000, default = 100, vals = {11, 12}}
-fields[#fields + 1] = {help = "profilesIntegral", row = 3, col = 2, min = 0, max = 1000, default = 50, vals = {19, 20}}
+fields[#fields + 1] = {help = "profilesIntegral", row = 1, col = 2, min = 0, max = 1000, default = 120, vals = {3, 4}}
+fields[#fields + 1] = {help = "profilesIntegral", row = 2, col = 2, min = 0, max = 1000, default = 120, vals = {11, 12}}
+fields[#fields + 1] = {help = "profilesIntegral", row = 3, col = 2, min = 0, max = 1000, default = 150, vals = {19, 20}}
 
 -- O
-fields[#fields + 1] = {help = "profilesHSI", row = 1, col = 3, min = 0, max = 1000, default = 25, vals = {31, 32}}
-fields[#fields + 1] = {help = "profilesHSI", row = 2, col = 3, min = 0, max = 1000, default = 25, vals = {33, 34}}
+fields[#fields + 1] = {help = "profilesHSI", row = 1, col = 3, min = 0, max = 1000, default = 40, vals = {31, 32}}
+fields[#fields + 1] = {help = "profilesHSI", row = 2, col = 3, min = 0, max = 1000, default = 40, vals = {33, 34}}
 
 -- D
-fields[#fields + 1] = {help = "profilesDerivative", row = 1, col = 4, min = 0, max = 1000, default = 10, vals = {5, 6}}
-fields[#fields + 1] = {help = "profilesDerivative", row = 2, col = 4, min = 0, max = 1000, default = 20, vals = {13, 14}}
-fields[#fields + 1] = {help = "profilesDerivative", row = 3, col = 4, min = 0, max = 1000, default = 10, vals = {21, 22}}
+fields[#fields + 1] = {help = "profilesDerivative", row = 1, col = 4, min = 0, max = 1000, default = 30, vals = {5, 6}}
+fields[#fields + 1] = {help = "profilesDerivative", row = 2, col = 4, min = 0, max = 1000, default = 50, vals = {13, 14}}
+fields[#fields + 1] = {help = "profilesDerivative", row = 3, col = 4, min = 0, max = 1000, default = 20, vals = {21, 22}}
 
 -- F
 fields[#fields + 1] = {help = "profilesFeedforward", row = 1, col = 5, min = 0, max = 1000, default = 100, vals = {7, 8}}
