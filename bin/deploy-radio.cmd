2fd4bf23035a32b934a0e316286255e25480b78d
Rob Thomson <rob@marotori.com>
2024-08-18 07:56:34 +0100

    Update preferences layout and introduce new options

--- bin/deploy-radio.cmd
+++ bin/deploy-radio.cmd
@@ -1,2 +1,2 @@
 
-rsync -rva --delete %DEV_RF2ETHOS_GIT_SRC%/scripts/rf2ethos  %DEV_RADIO_SRC%/scripts/
+rsync -rva --delete  --exclude=*.cfg  %DEV_RF2ETHOS_GIT_SRC%/scripts/rf2ethos  %DEV_RADIO_SRC%/scripts/
