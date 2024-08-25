c7d0a23999e033bc684f4845b7bec608f5b89be8
Rob Thomson <rob@marotori.com>
2024-08-25 07:50:38 +0100

    remove inline function to make easier to read

--- scripts/rf2ethos/pages/servos.lua
+++ scripts/rf2ethos/pages/servos.lua
@@ -21,203 +21,214 @@ fields[#fields + 1] = {t = "Rate", help = "servoRate", min = 50, max = 5000, def
 fields[#fields + 1] = {t = "Speed", help = "servoSpeed", min = 0, max = 60000, default = 0, unit = "ms", vals = {14, 15}}
 fields[#fields + 1] = {t = "Flags", help = "servoSpeed", hideme=true, min = 0, max = 60000, default = 0, unit = "ms", vals = {16, 18}}
 
-return {
-    read = 120, -- msp_SERVO_CONFIGURATIONS
-    write = 212, -- msp_SET_SERVO_CONFIGURATION
-    title = "Servos",
-    reboot = false,
-    eepromWrite = true,
-    minBytes = 33,
-    labels = labels,
-    fields = fields,
-    simulatorResponse = {
-        4, 180, 5, 12, 254, 244, 1, 244, 1, 244, 1, 144, 0, 0, 0, 1, 0, 160, 5, 12, 254, 244, 1, 244, 1, 244, 1, 144, 0, 0, 0, 1, 0, 14, 6, 12, 254, 244, 1, 244, 1, 244, 1, 144, 0, 0, 0, 0, 0, 120, 5,
-        212, 254, 44, 1, 244, 1, 244, 1, 77, 1, 0, 0, 0, 0
-    },
-    postRead = function(self)
-        -- rf2ethos.utils.log("postRead")
-        self.servoCount = self.values[1]
-        if rf2ethos.lastServoCount ~= self.servoCount then rf2ethos.lastServoCount = self.servoCount end
-        -- self.fields[1].max = servoCount - 1
-        self.servoConfiguration = {}
-        for i = 1, self.servoCount do
-            self.servoConfiguration[i] = {}
-            for j = 1, 16 do self.servoConfiguration[i][j] = self.values[1 + (i - 1) * 16 + j] end
-        end
-        if rf2ethos.lastChangedServo == nil then rf2ethos.lastChangedServo = 1 end
-        self.setValues(self, rf2ethos.lastChangedServo)
-        self.minBytes = 1 + 16
-    end,
-    postLoad = function(self)
-        -- rf2ethos.utils.log("postLoad")
-		
+
+
+local function postRead(self)
+
+	self.servoCount = self.values[1]
+	if rf2ethos.lastServoCount ~= self.servoCount then rf2ethos.lastServoCount = self.servoCount end
+
+	self.servoConfiguration = {}
+	for i = 1, self.servoCount do
+		self.servoConfiguration[i] = {}
+		for j = 1, 16 do self.servoConfiguration[i][j] = self.values[1 + (i - 1) * 16 + j] end
+	end
+	if rf2ethos.lastChangedServo == nil then rf2ethos.lastChangedServo = 1 end
+	self.setValues(self, rf2ethos.lastChangedServo)
+	self.minBytes = 1 + 16
+end
+
+local function postLoad(self)
+
 		if rf2ethos.config.ethosRunningVersion >= 1415 then
 			rf2ethos.Page.servoCenterFocusAllOff(self)	
 		end	
 		
 		rf2ethos.triggers.isReady = true
-    end,
-    setValues = function(self, servoIndex)
-        -- rf2ethos.utils.log("setValues")
-        self.values = {}
-        self.values[1] = servoIndex - 1
-        for i = 1, 16 do self.values[1 + i] = self.servoConfiguration[servoIndex][i] end
-    end,
-    servoChanged = function(self, servoIndex)
-        -- rf2ethos.utils.log("servoChanged")
-        rf2ethos.lastChangedServo = servoIndex
-        self.setValues(self, rf2ethos.lastChangedServo)
-        rf2ethos.dataBindFields()
-    end,	
-	servoCenterFocusAllOn = function(self)
-		for i = 0, #self.servoConfiguration do
-				local servoIndex = i
-				local message = {
-					command = 193, -- MSP_SET_SERVO_OVERRIDE
-					payload = { servoIndex }
-				}
-				rf2ethos.mspHelper.writeU16(message.payload, 0)
-				rf2ethos.mspQueue:add(message)	
-		end		
-		rf2ethos.triggers.isReady = true
-	end,
-	servoCenterFocusAllOff = function(self)
-	
-	
-		for i = 0, #self.servoConfiguration do
-				local servoIndex = i
-				local message = {
-					command = 193, -- MSP_SET_SERVO_OVERRIDE
-					payload = { servoIndex }
-				}
-				rf2ethos.mspHelper.writeU16(message.payload, 2001)
-				rf2ethos.mspQueue:add(message)	
-		end		
-		rf2ethos.triggers.isReady = true
-	end,
-	servoCenterFocus = function(self)
-			if inFocus == false then
-				rf2ethos.Page.servoCenterFocusOn(self)
-				inFocus = true
-			else
-				rf2ethos.Page.servoCenterFocusOff(off)
-				inFocus = false
-			end	
-    end,
-	servoCenterFocusOn = function(self)
-		local servoIndex = rf2ethos.Page.fields[1].value -1
-		
-		local message = {
-			command = 193, -- MSP_SET_SERVO_OVERRIDE
-			payload = { servoIndex }
-		}
-		rf2ethos.mspHelper.writeU16(message.payload, 0)
-		rf2ethos.mspQueue:add(message)		
-	end,	
-	servoCenterFocusOff = function(self)
-		local servoIndex = rf2ethos.Page.fields[1].value -1
-		
-		local message = {
-			command = 193, -- MSP_SET_SERVO_OVERRIDE
-			payload = { servoIndex }
-		}
-		rf2ethos.mspHelper.writeU16(message.payload, 2001)
-		rf2ethos.mspQueue:add(message)		
-	end,		
-	servoCenterTriggerChange = function(self)
-		if inOverRideAll == true or inFocus == true then
-			triggerCenterChange = true
-			currentServoCenter = math.floor(rf2ethos.Page.fields[2].value)
-		else
-			triggerCenterChange = false
-		end
-	end,
-	servoCenterChanged = function(self)
+end
 
-			local servoIndex = rf2ethos.Page.fields[1].value -1
-			local servoCenter = math.floor(rf2ethos.Page.fields[2].value)
-			local servoMin = math.floor(rf2ethos.Page.fields[3].value)		
-			local servoMax = math.floor(rf2ethos.Page.fields[4].value)
-			local servoScaleNeg = math.floor(rf2ethos.Page.fields[5].value)
-			local servoScalePos = math.floor(rf2ethos.Page.fields[6].value)
-			local servoRate = math.floor(rf2ethos.Page.fields[7].value)
-			local servoSpeed = math.floor(rf2ethos.Page.fields[8].value)
-			local servoFlags = math.floor(rf2ethos.Page.fields[9].value)
-			
+local function setValues(self, servoIndex)
+	self.values = {}
+	self.values[1] = servoIndex - 1
+	for i = 1, 16 do self.values[1 + i] = self.servoConfiguration[servoIndex][i] end
+end
+
+local function servoChanged(self, servoIndex)
+	rf2ethos.lastChangedServo = servoIndex
+	self.setValues(self, rf2ethos.lastChangedServo)
+	rf2ethos.dataBindFields()
+end
+
+local function servoCenterFocusAllOn(self)
+	for i = 0, #self.servoConfiguration do
+			local servoIndex = i
 			local message = {
-				command = 212, -- MSP_SET_SERVO_CONFIGURATION
-				payload = {}
+				command = 193, -- MSP_SET_SERVO_OVERRIDE
+				payload = { servoIndex }
 			}
-			rf2ethos.mspHelper.writeU8(message.payload, servoIndex)
-			rf2ethos.mspHelper.writeU16(message.payload, servoCenter)
-			rf2ethos.mspHelper.writeU16(message.payload, servoMin)
-			rf2ethos.mspHelper.writeU16(message.payload, servoMax)
-			rf2ethos.mspHelper.writeU16(message.payload, servoScaleNeg)
-			rf2ethos.mspHelper.writeU16(message.payload, servoScalePos)
-			rf2ethos.mspHelper.writeU16(message.payload, servoRate)
-			rf2ethos.mspHelper.writeU16(message.payload, servoSpeed)
-			rf2ethos.mspHelper.writeU16(message.payload, servoFlags) 
+			rf2ethos.mspHelper.writeU16(message.payload, 0)
+			rf2ethos.mspQueue:add(message)	
+	end		
+	rf2ethos.triggers.isReady = true
+end
 
-			if rf2ethos.config.mspTxRxDebug == true or rf2ethos.config.logEnable == true then
-			   local logData = "{" .. rf2ethos.utils.joinTableItems(message.payload, ", ") .. "}"
-			   
-			   rf2ethos.utils.log(logData)
-			   
-				if rf2ethos.config.mspTxRxDebug == true then
-						print(logData)
-				end			   
-			   
-			end   
-			lastSetServoCenter = servoCenter
-			print("Setting center to: " .. servoCenter)
-			rf2ethos.mspQueue:add(message)			
+
+local function servoCenterFocusAllOff(self)
+	for i = 0, #self.servoConfiguration do
+			local servoIndex = i
+			local message = {
+				command = 193, -- MSP_SET_SERVO_OVERRIDE
+				payload = { servoIndex }
+			}
+			rf2ethos.mspHelper.writeU16(message.payload, 2001)
+			rf2ethos.mspQueue:add(message)	
+	end		
+	rf2ethos.triggers.isReady = true
+end
+
+local function servoCenterFocus(self)
+	if inFocus == false then
+		rf2ethos.Page.servoCenterFocusOn(self)
+		inFocus = true
+	else
+		rf2ethos.Page.servoCenterFocusOff(off)
+		inFocus = false
+	end	
+end
+
+local function servoCenterFocusOn(self)
+	local servoIndex = rf2ethos.Page.fields[1].value -1
 	
-    end,
-    toolButton = function(self)
+	local message = {
+		command = 193, -- MSP_SET_SERVO_OVERRIDE
+		payload = { servoIndex }
+	}
+	rf2ethos.mspHelper.writeU16(message.payload, 0)
+	rf2ethos.mspQueue:add(message)		
+end
 
-		local buttons = {
-					{
-						label = "        OK        ",
-						action = function()
-							
-							--we cant launch the loader here to se rely on the modules
-							--wakup function to do this
-							triggerOverRideAll = true
-							return true
-						end
-					}, {
-						label = "CANCEL",
-						action = function()
-							return true
-						end
-					}
+local function servoCenterFocusOff(self)
+	local servoIndex = rf2ethos.Page.fields[1].value -1
+	
+	local message = {
+		command = 193, -- MSP_SET_SERVO_OVERRIDE
+		payload = { servoIndex }
+	}
+	rf2ethos.mspHelper.writeU16(message.payload, 2001)
+	rf2ethos.mspQueue:add(message)		
+end
+
+local function servoCenterTriggerChange(self)
+	if inOverRideAll == true or inFocus == true then
+		triggerCenterChange = true
+		currentServoCenter = math.floor(rf2ethos.Page.fields[2].value)
+	else
+		triggerCenterChange = false
+	end
+end
+
+local function servoCenterChanged(self)
+
+	local servoIndex = rf2ethos.Page.fields[1].value -1
+	local servoCenter = math.floor(rf2ethos.Page.fields[2].value)
+	local servoMin = math.floor(rf2ethos.Page.fields[3].value)		
+	local servoMax = math.floor(rf2ethos.Page.fields[4].value)
+	local servoScaleNeg = math.floor(rf2ethos.Page.fields[5].value)
+	local servoScalePos = math.floor(rf2ethos.Page.fields[6].value)
+	local servoRate = math.floor(rf2ethos.Page.fields[7].value)
+	local servoSpeed = math.floor(rf2ethos.Page.fields[8].value)
+	local servoFlags = math.floor(rf2ethos.Page.fields[9].value)
+	
+	local message = {
+		command = 212, -- MSP_SET_SERVO_CONFIGURATION
+		payload = {}
+	}
+	rf2ethos.mspHelper.writeU8(message.payload, servoIndex)
+	rf2ethos.mspHelper.writeU16(message.payload, servoCenter)
+	rf2ethos.mspHelper.writeU16(message.payload, servoMin)
+	rf2ethos.mspHelper.writeU16(message.payload, servoMax)
+	rf2ethos.mspHelper.writeU16(message.payload, servoScaleNeg)
+	rf2ethos.mspHelper.writeU16(message.payload, servoScalePos)
+	rf2ethos.mspHelper.writeU16(message.payload, servoRate)
+	rf2ethos.mspHelper.writeU16(message.payload, servoSpeed)
+	rf2ethos.mspHelper.writeU16(message.payload, servoFlags) 
+
+	if rf2ethos.config.mspTxRxDebug == true or rf2ethos.config.logEnable == true then
+	   local logData = "{" .. rf2ethos.utils.joinTableItems(message.payload, ", ") .. "}"
+	   
+	   rf2ethos.utils.log(logData)
+	   
+		if rf2ethos.config.mspTxRxDebug == true then
+				print(logData)
+		end			   
+	   
+	end   
+	lastSetServoCenter = servoCenter
+	print("Setting center to: " .. servoCenter)
+	rf2ethos.mspQueue:add(message)			
+
+end
+
+local function toolButton(self)
+
+	local buttons = {
+				{
+					label = "        OK        ",
+					action = function()
+						
+						--we cant launch the loader here to se rely on the modules
+						--wakup function to do this
+						triggerOverRideAll = true
+						return true
+					end
+				}, {
+					label = "CANCEL",
+					action = function()
+						return true
+					end
 				}
-				local msg
-				local title
-				if inOverRideAll == false then
-					title = "Activate"
-					message = "Set all servos to their configured center position"
-				else
-					title = "Disable"
-					message = "Return control of the servos to the flight controller"
-				end
-				
-				form.openDialog({
-					width = nil,
-					title = title,
-					message = message,
-					buttons = buttons,
-					wakeup = function()
-					end,
-					paint = function()
-					end,
-					options = TEXT_LEFT
-				})
+			}
+			local msg
+			local title
+			if inOverRideAll == false then
+				title = "Activate"
+				message = "Set all servos to their configured center position"
+			else
+				title = "Disable"
+				message = "Return control of the servos to the flight controller"
+			end
+			
+			form.openDialog({
+				width = nil,
+				title = title,
+				message = message,
+				buttons = buttons,
+				wakeup = function()
+				end,
+				paint = function()
+				end,
+				options = TEXT_LEFT
+			})
 
+end
 
+local function onMenuExit(self)
 
-    end,
-	wakeup =  function(self)
+			if inOverRideAll == true then
+				inOverRideAll = false
+				rf2ethos.dialogs.progressDisplay = true
+				rf2ethos.dialogs.progressWatchDog = os.clock()
+				rf2ethos.dialogs.progress = form.openProgressDialog("Servo overide...", "Disabling servo overide.")
+				rf2ethos.dialogs.progress:value(0)
+				rf2ethos.dialogs.progress:closeAllowed(false)
+			
+				rf2ethos.Page.servoCenterFocusAllOff(self)			
+				rf2ethos.triggers.closeProgressLoader = true
+			end	
+	
+	end
+
+local function wakeup(self)
 	
 
 		if rf2ethos.mspQueue:isProcessed() then
@@ -257,22 +268,35 @@ return {
 				inOverRideAll = false
 			end
 		end
-	end,
-	onMenuExit = function(self)
+	end
 
-			if inOverRideAll == true then
-				inOverRideAll = false
-				rf2ethos.dialogs.progressDisplay = true
-				rf2ethos.dialogs.progressWatchDog = os.clock()
-				rf2ethos.dialogs.progress = form.openProgressDialog("Servo overide...", "Disabling servo overide.")
-				rf2ethos.dialogs.progress:value(0)
-				rf2ethos.dialogs.progress:closeAllowed(false)
-			
-				rf2ethos.Page.servoCenterFocusAllOff(self)			
-				rf2ethos.triggers.closeProgressLoader = true
-			end	
-	
-	end,
+return {
+    read = 120, -- msp_SERVO_CONFIGURATIONS
+    write = 212, -- msp_SET_SERVO_CONFIGURATION
+    title = "Servos",
+    reboot = false,
+    eepromWrite = true,
+    minBytes = 33,
+    labels = labels,
+    fields = fields,
+    simulatorResponse = {
+        4, 180, 5, 12, 254, 244, 1, 244, 1, 244, 1, 144, 0, 0, 0, 1, 0, 160, 5, 12, 254, 244, 1, 244, 1, 244, 1, 144, 0, 0, 0, 1, 0, 14, 6, 12, 254, 244, 1, 244, 1, 244, 1, 144, 0, 0, 0, 0, 0, 120, 5,
+        212, 254, 44, 1, 244, 1, 244, 1, 77, 1, 0, 0, 0, 0
+    },
+    postRead = postRead,
+    postLoad = postLoad,
+    setValues = setValues,
+    servoChanged = servoChanged,	
+	servoCenterFocusAllOn = servoCenterFocusAllOn,
+	servoCenterFocusAllOff = servoCenterFocusAllOff,
+	servoCenterFocus = servoCenterFocus,
+	servoCenterFocusOn = servoCenterFocusOn,	
+	servoCenterFocusOff = servoCenterFocusOff,		
+	servoCenterTriggerChange = servoCenterTriggerChange,
+	servoCenterChanged = servoCenterChanged,
+    toolButton = toolButton,
+	wakeup =  wakeup,
+	onMenuExit = onMenuExit,
 
 	
 }
