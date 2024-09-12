101ff3da071917e7a5e264515d4fa785b0c8872d
Rob Thomson <rob@marotori.com>
2024-09-12 09:11:38 +0100

    Add titlecase lib

--- scripts/rf2ethos/lib/utils.lua
+++ scripts/rf2ethos/lib/utils.lua
@@ -9,6 +9,13 @@ function utils.getRssiSensor()
     end
 end
 
+function utils.titleCase(str)
+    return str:gsub("(%a)([%w_']*)", function(first, rest)
+        return first:upper() .. rest:lower()
+    end)
+end
+
+
 function utils.stringInArray(array, s)
     for i, value in ipairs(array) do if value == s then return true end end
     return false
