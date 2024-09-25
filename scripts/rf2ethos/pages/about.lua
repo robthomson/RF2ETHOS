ac600d4dd15187b44956b88e3f0e1df32e108a3e
Rob Thomson <rob@marotori.com>
2024-09-25 11:51:23 +0100

    Add transport type to about page

--- scripts/rf2ethos/pages/about.lua
+++ scripts/rf2ethos/pages/about.lua
@@ -43,9 +43,10 @@ end
 fields[1] = {t = "Version", value = version, type = displayType, disable = disableType, position = displayPos}
 fields[2] = {t = "Ethos Version", value = ethosVersion, type = displayType, disable = disableType, position = displayPos}
 fields[3] = {t = "MSP Version", value = apiVersion, type = displayType, disable = disableType, position = displayPos}
-fields[4] = {t = "Supported MSP Versions", value = supportedMspVersion, type = displayType, disable = disableType, position = displayPos}
-fields[5] = {t = "Compilation", value = compilation, type = displayType, disable = disableType, position = displayPos}
-fields[6] = {t = "Simulation", value = simulation, type = displayType, disable = disableType, position = displayPos}
+fields[4] = {t = "MSP Transport", value = string.upper(rf2ethos.protocol.mspProtocol), type = displayType, disable = disableType, position = displayPos}
+fields[5] = {t = "Supported MSP Versions", value = supportedMspVersion, type = displayType, disable = disableType, position = displayPos}
+fields[6] = {t = "Compilation", value = compilation, type = displayType, disable = disableType, position = displayPos}
+fields[7] = {t = "Simulation", value = simulation, type = displayType, disable = disableType, position = displayPos}
 
 function readMSP()
     rf2ethos.triggers.isReady = true
